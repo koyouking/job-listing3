c7a31e24ec93cacb1e19a7924378ccc446b59731
kOyou <kingrulen@gmail.com>
2017-01-15 17:43:07 +0800

    correct order by wage upper bound

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,7 +9,7 @@ class JobsController < ApplicationController
     @jobs = case params[:order]
     when 'by_lower_bound'
       Job.published.order('wage_lower_bound DESC')
-    when
+    when 'by_upper_bound'
       Job.published.order('wage_upper_bound DESC')
     else
       Job.published.recent
